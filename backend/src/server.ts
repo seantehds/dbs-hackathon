fe3d764187b482d474c040415d1f794d50877e1a
sean <seanteh2001@gmail.com>
2026-01-10 11:18:31 +0800

    feat: app.ts and server.ts

---
+++ backend/src/server.ts
@@ -0,0 +1,18 @@
+import app from "./app";
+import { Response } from "express";
+
+const PORT = process.env.PORT || 3000;
+
+// -------- API ROUTES --------
+
+// test
+app.get("/api/test", (_req, res: Response) => {
+  console.log("test called")
+  res.json({ api: "works hooray" });
+});
+
+// -------- START SERVER --------
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
