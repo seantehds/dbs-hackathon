e2871854d78b678ef14fd89e8a7e20eccf9563e5
zhengwinter <e0958239@u.nus.edu>
2026-01-10 11:16:40 +0800

    Init button definitions

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,9 +1,24 @@
+import PropTypes from 'prop-types'
 const Button = () => {
   return (
-    <div>
-      
-    </div>
+    <button 
+        onClick={onClick}
+        style = {{backgroundColor: color }} 
+        className = 'btn'
+        >
+         {text}
+    </button>
   )
+}
+
+Button.defaultProps = {
+    color: 'steelblue'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
 }
 
 export default Button
