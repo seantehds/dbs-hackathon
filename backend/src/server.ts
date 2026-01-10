268885450ad86b43e021467963905382f7ed6e2b
NeoJM <jingmin.neo.2020@scis.smu.edu.sg>
2026-01-10 12:11:12 +0800

    Revert "Merge branch 'main' of https://github.com/seantehds/dbs-hackathon"
    This reverts commit 7d4123f36c16b1961f3368c6e34b9bc89825b4d4, reversing
    changes made to cdc0a2e0c3386d71e8a71acfde70fbab0ae59c2e.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,6 @@
-import mongoose from "mongoose";
 import app from "./app";
 import { Response } from "express";
-import dotenv from "dotenv";
 
-dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 // -------- API ROUTES --------
@@ -16,8 +13,6 @@ app.get("/api/test", (_req, res: Response) => {
 
 // -------- START SERVER --------
 
-app.listen(PORT, async () => {
+app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
-  console.log(process.env.ATLAS_URI)
-  await mongoose.connect(process.env.ATLAS_URI as string);
 });
