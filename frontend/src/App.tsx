9187debe340efd1cdc7bd8f71b980a5929deabaa
zhengwinter <e0958239@u.nus.edu>
2026-01-10 15:56:05 +0800

    Update data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,38 +13,38 @@ const outstandingAmount = [
 const getData = () => [
   {
     name: "Jane Cooper",
-    role: "Admin",
+    role: "You are owed $150",
     imgUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
   },
   {
     name: "Cody Fisher",
-    role: "Owner",
+    role: "You owe $20",
     imgUrl:
       "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
   },
   {
     name: "Esther Howard",
-    role: "Member",
+    role: "You are owed $5",
     imgUrl:
       "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
   },
   {
     name: "Jenny Wilson",
-    role: "Member",
+    role: "You owe $250",
     imgUrl:
       "https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
   },
   {
     name: "Kristin Watson",
-    role: "Admin",
+    role: "You owe $1000",
     imgUrl:
       "https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
   },
   {
     name: "Cameron Williamson",
     //statement: `You owe A ${net}`
-    role: "Member",
+    role: "You are owed $300",
     imgUrl:
       "https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
   },
