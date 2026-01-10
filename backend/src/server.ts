e6e98acac280ef815ab5dbe73cad602c1a8ea3cb
matochichap <ruijia.chan@gmail.com>
2026-01-10 12:08:25 +0800

    add mongodb connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,9 @@
+import mongoose from "mongoose";
 import app from "./app";
 import { Response } from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 // -------- API ROUTES --------
@@ -13,6 +16,8 @@ app.get("/api/test", (_req, res: Response) => {
 
 // -------- START SERVER --------
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(process.env.ATLAS_URI)
+  await mongoose.connect(process.env.ATLAS_URI as string);
 });
